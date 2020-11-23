c94cd24e989df077c4a9291df440b036faa8d0fe
Blaise <blaze.kalonda@gmail.com>
2020-11-23 02:34:56 +0100

    custom card

---
+++ src/components/card.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+import clsx from 'clsx';
+import ship from '../img/Ship.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 300,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
+
+const  CustomCard = (props) => {
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={ship}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" onClick={handleExpandClick}>
+          expand
+        </Button>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <CardContent>
+          <Typography>
+            kdaksklld
+          </Typography>
+      </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+export default CustomCard;
--- src/container/home.js
+++ src/container/home.js
@@ -1,7 +1,8 @@
-import {React,useState} from 'react';
+import {React,useState, useEffect} from 'react';
 import Bar from '../components/appBar';
 import PopUp from '../components/popUp';
 import {auth,db} from '../dataBase/firebase';
+import CustomCard from '../components/card';
 
 const handleLogOut = () => {
     auth.signOut();
@@ -17,7 +18,8 @@ const Home = () => {
   // const books = boat.collection('books'); //à ajouter title et author
   const [open, setOpen] = useState(false);
   const [boatName,setBoatName] = useState('');
-    
+  const snap = [];
+
       const handleClickOpen = () => {
         setOpen(true);
       };
@@ -30,25 +32,48 @@ const Home = () => {
           setBoatName('')
       };
 
-      const  addBoat =  async () => {
+      const  createBoat =  async () => {
 
         clearInputs();
         handleClose();
 
         const data = {
-          'boatname': boatName
+          boatname: boatName
         }
 
         await boats.doc().set(data,{ merge: true })
     }
 
+    // TODO loop render custom card component
+    const getdocument = async () => {
+
+      await  boats.get().then(querySnapshot => {
+        querySnapshot.forEach((doc) => {
+        snap.push(doc.data());
+        console.log(snap);
+       })
+     });
+
+    
+    }
+
+    useEffect(() => {
+      getdocument();
+    },[])
+
 
     return (
     <div>
       <Bar handleLogOut={handleLogOut} handleClickOpen={handleClickOpen}/>
-        <PopUp open={open} handleClose={handleClose} save={addBoat}
+        <PopUp open={open} handleClose={handleClose} save={createBoat}
          boatName={boatName} setBoatName={setBoatName}/>
-
+         {/* <CustomCard title='maryJane'/> */}
+         { 
+            snap.map((d,i)=> {
+            return  <CustomCard key={i} title={d}/>
+             })
+          
+        }
     </div>
     );
 }
